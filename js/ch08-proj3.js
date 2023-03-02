548c9501d5feb06c9672061268e37a563a6016a6
hillaryke <tenskap@gmail.com>
2023-03-02 13:16:37 +0300

    change styling

--- js/ch08-proj3.js
+++ js/ch08-proj3.js
@@ -31,7 +31,7 @@ function CompanyCard(company) {
       return formatter.format(num);
    };
 
-   CompanyCard.prototype.outputCard = function() {
+   CompanyCard.prototype.outputCard = function () {
       const marketCap50Formatted = this.billions(this.marketCap50);
       const marketCap200Formatted = this.billions(this.marketCap200);
       const revenueFormatted = this.billions(this.revenue);
@@ -40,33 +40,34 @@ function CompanyCard(company) {
       const tagsList = this.tags.map(tag => `<li>${tag}</li>`).join('');
 
       return `
-    <div class="card">
-      <h2>${this.symbol}</h2>
-      <h3>${this.name}</h3>
-      <ul>
-        <li>50 Day Moving Avg: ${this.currency(this.day50)}</li>
-        <li>200 Day Moving Avg: ${this.currency(this.day200)}</li>
-        <li>Operating Revenue: ${revenueFormatted}</li>
-        <li>Market Cap (50 Day): ${marketCap50Formatted}</li>
-        <li>Market Cap (200 Day): ${marketCap200Formatted}</li>
-        <li>Equity: ${equityFormatted}</li>
-        <li>Tags:</li>
-        <ul>
-          ${tagsList}
-        </ul>
-      </ul>
-    </div>
+         <article class="card">
+               <h2>AAPL - Apple Inc.</h2>
+               <div>
+                 <p>Share Price (50day avg): <span>$182.26</span></p>
+                 <p>Share Price (200day avg): <span>$190.30</span></p>
+                 <p>Market Cap (50day avg): <span>$859,406,932,800.00</span></p>
+                 <p>Market Cap (200day avg): <span>$897,317,784,000.00</span></p>
+                 <p>Net Revenue: <span>$32,031,000,000.00</span></p>
+                 <p>Shareholder Equity: <span>$117,892,000,000.00</span></p>
+               </div>
+               <footer>
+                   <small>Technology</small> 
+                   <small>Consumer Electronics</small>
+                   <small>Computer Hardware</small>
+               </footer>
+            </article>
   `;
-   }
+   };
 }
+
 function outputCompanyCards(companiesObj) {
 
    const newMarkup = companiesObj.map(company => {
-      const card = new CompanyCard(company)
-      const cardMarkup = card.outputCard()
+      const card = new CompanyCard(company);
+      const cardMarkup = card.outputCard();
       return cardMarkup;
    }).join('');
-   document.getElementById('company-cards').innerHTML = newMarkup
+   document.getElementById('company-cards').innerHTML = newMarkup;
 }
 
 outputCompanyCards(companiesObj);
